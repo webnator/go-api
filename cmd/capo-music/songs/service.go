786071ff3dba10b91d41c91b012af68f68ff4e77
Williams Aguilera <williams.aguilera@cloud-iq.com>
2019-10-29 14:57:48 +0100

    ADD: Continuing the party

--- cmd/capo-music/songs/service.go
+++ cmd/capo-music/songs/service.go
@@ -2,14 +2,7 @@ package songs
 
 var dao *SongDAO = NewSongDAO()
 
-type SongsService struct{}
-
-// NewSongsService creates a new SongsService with the given user DAO.
-func NewSongsService() *SongsService {
-	return &SongsService{}
-}
-
-// GetAll just retrieves user using User DAO, here can be additional logic for processing data retrieved by DAOs
+// GetAll retrieves all songs in the DB.
 func GetAll() (*[]SongModel, error) {
 	return dao.getAll()
 }
--- cmd/capo-music/songs/song.model.go
+++ cmd/capo-music/songs/song.model.go
@@ -1,5 +1,23 @@
 package songs
 
+// SongModel represents the song entity in the db.
 type SongModel struct {
-	Title string `json:"title"`
+	Title    string      `json:"title"`
+	Slug     string      `json:"slug"`
+	Visible  bool        `json:"visible"`
+	Lyrics   string      `json:"lyrics"`
+	Category []string    `json:"category"`
+	Media    mediaModel  `json:"media"`
+	Composer artistModel `json:"composer"`
+	Singer   artistModel `json:"singer"`
+}
+
+type mediaModel struct {
+	AudioLink string `json:"audioLink"`
+	VideoLink string `json:"videoLink"`
+}
+
+type artistModel struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
 }
